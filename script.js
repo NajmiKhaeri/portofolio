3195c2cec6911040d9e790b1e505805447d95812
Najmi Khaeri Arrisa Putra <69677376+NajmiKhaeri@users.noreply.github.com>
2023-11-16 05:59:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const animatedElements = document.querySelectorAll(".scroll-fade-in");
+
+  function checkScroll() {
+    animatedElements.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      if (elementTop < windowHeight - 50) {
+        element.classList.add("active");
+      }
+    });
+  }
+
+  // Cek awal saat halaman dimuat
+  checkScroll();
+
+  // Cek saat pengguna menggulir
+  window.addEventListener("scroll", checkScroll);
+});
